780cf8e2a1f4c855019a83203f3e765d89b7e710
AGiovanna73 <alda.04721@aluno.iffar.edu.br>
2026-01-07 09:09:58 -0300

    comentarios

--- estoques/Estoque.java
+++ estoques/Estoque.java
@@ -1,26 +1,33 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;//ler arquivos texto linha por linha
+import java.io.FileReader;//abrir arquivo para leitura
+import java.io.FileWriter;//abrir/criar arquivo para escrita
+import java.io.IOException;//tratar erros de arquivo
+import java.io.PrintWriter;//escrever texto no arquivo de forma simples
+import java.util.ArrayList;//vetor dinâmico para armazenar os itens do estoque
+import java.util.List;//vetor dinâmico para armazenar os itens do estoque
 
 public class Estoque {
 
-    private List<Itens> itens;
-    private static final String ARQUIVO = "Estoque.csv";
+    private List<Itens> itens;//lista guarda todos os itens do estoque,cada  é um objeto da classe Itens,vetor dinamico
+    private static final String ARQUIVO = "Estoque.csv";//Nome do arquivo onde o estoque será salvo.
 
     private static final String[] CATEGORIAS = {
         "Masculinos", "Femininos", "Infantil", "Calçados", "Diversos"
     };
 
-    public Estoque() {
+    public Estoque() {//Cria a lista vazia (ArrayList),lê os dados já salvos  CSV,faça q tds as categorias exis
         itens = new ArrayList<>();
         lerArquivo();
         inicializarCategorias();
     }
 
+
+
+//Percorre todas as categorias fixas
+//Verifica se ela já existe no estoque
+//Se não existir cria com qtd 0
+//Evita erro quando o usuário tenta usar uma categoria que não existe
+//Depois salva tudo no arquivo
     
     private void inicializarCategorias() {
         for (String categoria : CATEGORIAS) {
@@ -84,8 +91,8 @@ public class Estoque {
                 int quantidade = Integer.parseInt(dados[1]);
                 itens.add(new Itens(categoria, quantidade));
             }
-        } catch (IOException e) {
-            // arquivo ainda não existe (primeira execução)
+        } catch (IOException e) {//Se o arquivo não existir, o programa continua normalmente
+            // arquivo ainda não existe (primeira execução),e sem erro
         }
     }
 
@@ -100,3 +107,5 @@ public class Estoque {
         }
     }
 }
+
+//List<Itens> :simula memória
