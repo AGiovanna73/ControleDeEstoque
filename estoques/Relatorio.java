1ed2726908c3e26a96a44367bc70eab4ae8c3ef1
AGiovanna73 <alda.04721@aluno.iffar.edu.br>
2026-01-07 09:21:42 -0300

    mais comentarios

--- estoques/Relatorio.java
+++ estoques/Relatorio.java
@@ -4,23 +4,25 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Map; //estrutura chave → valor (categoria → quantidade),Aqui o Map substitui vetores/matrizes, facilitando o cruzamento de dados.
 
 public class Relatorio {
 
     public static void gerarRelatorioFinal() {
 
+      //  Cada arquivo CSV é lido,Os dados são armazenados tipo mapas:chave vai para categoria,valor vai para quantidade 
+        
         Map<String, Integer> estoque = lerCSV("Estoque.csv");
         Map<String, Integer> emprestados = lerCSV("EmprestimosResumo.csv");
         Map<String, Integer> doados = lerCSV("Doacoes.csv");
 
-        try (PrintWriter pw = new PrintWriter(new FileWriter("RelatorioFinal.csv"))) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter("RelatorioFinal.csv"))) {//Cria o arquivo RelatorioFinal.csv;try-with-resources fecha o arquivo automaticamente
 
-            pw.println("Categoria,Em Estoque,Emprestado,Doado,Disponível");
+            pw.println("Categoria,Em Estoque,Emprestado,Doado,Disponível");//Define as colunas do relatório
 
-            for (String categoria : estoque.keySet()) {
+            for (String categoria : estoque.keySet()) {//Percorre todas as categorias existentes no estoque; keySet() retorna apenas as chaves do Map
 
-                int qtdEstoque = estoque.getOrDefault(categoria, 0);
+                int qtdEstoque = estoque.getOrDefault(categoria, 0);//se a categoria existir retorna o valor se nao retorna 0
                 int qtdEmprestado = emprestados.getOrDefault(categoria, 0);
                 int qtdDoado = doados.getOrDefault(categoria, 0);
 
@@ -39,11 +41,12 @@ public class Relatorio {
     }
 
  
-    private static Map<String, Integer> lerCSV(String arquivo) {
+    private static Map<String, Integer> lerCSV(String arquivo) {//Método genérico para leitura de qualquer CSV simples
 
-        Map<String, Integer> dados = new HashMap<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+        Map<String, Integer> dados = new HashMap<>();//cria o map(mapa) e armazena os dados do arquivo
+
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {//Abre o arquivo para leitura.
             String linha;
 
             while ((linha = br.readLine()) != null) {
@@ -51,7 +54,7 @@ public class Relatorio {
                 String categoria = partes[0];
                 int quantidade = Integer.parseInt(partes[1]);
 
-                dados.put(categoria, quantidade);
+                dados.put(categoria, quantidade);//armazena os dados no Map
             }
 
         } catch (IOException e) {
@@ -61,3 +64,4 @@ public class Relatorio {
         return dados;
     }
 }
+
